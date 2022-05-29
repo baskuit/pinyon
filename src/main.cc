eea6d7f21025506f7438beec6a685fc963509f1a
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2022-05-29 06:19:45 -0700

    w/e

--- src/main.cc
+++ src/main.cc
@@ -5,17 +5,17 @@
 
 int main () {
 
-    int pp = 3;
+    int pp = 1;
     int length = 0;
 
     float total = 0;
-    int n = 100000;
+    int n = 1000;
 
 
+    ToyStateInfo info('u', pp, length, pp / (float) (pp + 1));
+    ToyState state_ = ToyState(info);
     for (int i = 0; i < n; ++i) {
-
-        ToyStateInfo info('u', pp, length, pp / (float) (pp + 1));
-        ToyState state = ToyState(info);
+        ToyState state = state_;
         state.rollout();
         total += state.info.payoff;
         //std::cout << state.info.payoff << std::endl;
