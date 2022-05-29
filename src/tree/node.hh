bb1e10e19972fca19b19f3b6b57ee82c0a9bfaf8
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2022-05-29 06:20:06 -0700

    nodes

---
+++ src/tree/node.hh
@@ -0,0 +1,63 @@
+#include "../model/model.hh"
+
+class Node {
+
+    Node* parent;
+    Node* child;
+    Node* next;
+
+    float cumulativeScore0 = 0.f;
+    float cumulativeScore1 = 0.f;
+    int visits = 0;
+
+public:
+    Node () :
+    parent(nullptr), child(nullptr), next(nullptr) {};
+    Node (Node* parent) :
+    parent(parent), child(nullptr), next(nullptr) {};
+    ~Node () {
+        delete child;
+        delete next;
+    }
+
+    void update (float u0, float u1) {
+        cumulativeScore0 += u0;
+        cumulativeScore1 += u1;
+        visits += 1;
+    }
+};
+
+class MatrixNode;
+
+class ChanceNode : public Node {
+
+    MatrixNode* parent;
+    MatrixNode* child;
+    ChanceNode* next;
+
+    Action action0;
+    Action action1;
+
+public:
+    ChanceNode (MatrixNode* parent, Action action0, Action action1) :
+    Node(), parent(parent), action0(action0), action1(action1) {}
+    
+    MatrixNode* access (StateTransitionData data);
+};
+
+class SearchStats {};
+
+class MatrixNode : public Node {
+
+    SearchStats stats;
+
+    Hash transitionKey;
+    Rational transitionProb;
+
+public:
+    MatrixNode () :
+    Node(), transitionKey(0) {}
+
+    ChanceNode* access (Action action0, Action action1);
+
+};
