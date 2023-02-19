7718538464ea80aa8658714bd0352850f3016e6e
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-02-19 00:22:35 -0700

    remove VERSION

--- test/gambit_link_test.cc
+++ test/gambit_link_test.cc
@@ -48,10 +48,6 @@ PrintCliques(const List<List<MixedStrategyProfile<T> > > &p_cliques,
 void PrintBanner(std::ostream &p_stream)
 {
   p_stream << "Compute Nash equilibria by enumerating extreme points\n";
-  p_stream << "Gambit version " VERSION ", Copyright (C) 1994-2022, The Gambit Project\n";
-  p_stream << "Enumeration code based on lrslib 6.2,\n";
-  p_stream << "Copyright (C) 1995-2016 by David Avis (avis@cs.mcgill.ca)\n";
-  p_stream << "This is free software, distributed under the GNU GPL\n\n";
 }
 
 void PrintHelp(char *progname)
