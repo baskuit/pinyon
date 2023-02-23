3354efbdfbd467fd1ef79662e5c46afab9f29fe2
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-02-23 04:58:59 -0700

    random trees

--- random_trees/random_trees.cc
+++ random_trees/random_trees.cc
@@ -1,10 +1,37 @@
 #include "tree_state.hh"
+#include "search/exp3p.hh"
+#include "search/matrix_ucb.hh"
+
+#include <iostream>
 
 int main () {
-    prng device(0);
-    SeedState<4> seed(device, 0, 4, 4);
-    Grow<MonteCarlo<SeedState<4>>> session(device);
-    MatrixNode<Grow<MonteCarlo<SeedState<4>>>>* root;
-    session.grow(seed, root);
+    prng device;
+
+    using TreeState = TreeState<4>;
+    using MatrixUCB = MatrixUCB<MonteCarlo<TreeState>>;
+    using Exp3p = Exp3p<MonteCarlo<TreeState>>;
+
+    MatrixNode<Grow<MonteCarlo<SeedState<4>>>> root;
+
+    TreeState tree(device, root, 6, 3, 3);
+    std::cout << "tree size: " << tree.root.count() << std::endl;
+    std::cout << "tree expected value: " << std::endl;
+    tree.root.stats.expected_value.print();
+
+    MatrixUCB matrix_ucb_session(device);
+    Exp3p exp3p_session(device);
+
+    MonteCarlo<TreeState> model(device);
+
+
+    double c_ucts[3] = {10, 5, 1};
+    for (int i = 0; i < 3; ++i) {
+        double c_uct = c_ucts[i];
+        matrix_ucb_session.c_uct = c_uct;
+        matrix_ucb_session.expl_threshold = .005;
+        MatrixNode<MatrixUCB> matrix_ucb_root;
+        matrix_ucb_session.search(10000, tree, model, matrix_ucb_root);
+    }   
+
     return 0;
 }
--- random_trees/tree_state.hh
+++ random_trees/tree_state.hh
@@ -6,24 +6,31 @@
 #include "grow.hh"
 
 template <int size>
-class TreeState : public State<size, int, int> {
-
-    prng& device;
+class TreeState : public State<size, int, int>
+{
+public:
+    prng &device;
     SeedState<size> seed;
-    MatrixNode<MonteCarlo<SeedState<size>>>* root;
-    MatrixNode<MonteCarlo<SeedState<size>>>* current;
+    MatrixNode<Grow<MonteCarlo<SeedState<size>>>>& root;
+    MatrixNode<Grow<MonteCarlo<SeedState<size>>>> *current;
 
+    TreeState(prng &device, MatrixNode<Grow<MonteCarlo<SeedState<size>>>>& root, int depth_bound, int rows, int cols) : State<size, int, int>(device), device(device), seed(SeedState<size>(device, depth_bound, rows, cols)), root(root)
+    {
+        current = &root;
+        Grow<MonteCarlo<SeedState<size>>> session(device);
+        session.grow(seed, current);
+    }
 
     typename TreeState::pair_actions_t get_legal_actions()
     {
-        typename TreeState::pairs_t legal_actions;
-        legal_actions.rows = this->rows;
-        legal_actions.cols = this->cols;
-        for (int i = 0; i < this->rows; ++i)
+        typename TreeState::pair_actions_t legal_actions;
+        legal_actions.rows = this->current->stats.expected_value.rows;
+        legal_actions.cols = this->current->stats.expected_value.cols;
+        for (int i = 0; i < legal_actions.rows; ++i)
         {
             legal_actions.actions0[i] = i;
         };
-        for (int j = 0; j < this->cols; ++j)
+        for (int j = 0; j < legal_actions.cols; ++j)
         {
             legal_actions.actions1[j] = j;
         };
@@ -32,13 +39,13 @@ class TreeState : public State<size, int, int> {
 
     void get_legal_actions(typename TreeState::pair_actions_t &legal_actions)
     {
-        legal_actions.rows = this->rows;
-        legal_actions.cols = this->cols;
-        for (int i = 0; i < this->rows; ++i)
+        legal_actions.rows = this->current->stats.expected_value.rows;
+        legal_actions.cols = this->current->stats.expected_value.cols;
+        for (int i = 0; i < legal_actions.rows; ++i)
         {
             legal_actions.actions0[i] = i;
         };
-        for (int j = 0; j < this->cols; ++j)
+        for (int j = 0; j < legal_actions.cols; ++j)
         {
             legal_actions.actions1[j] = j;
         };
@@ -46,14 +53,10 @@ class TreeState : public State<size, int, int> {
 
     typename TreeState::transition_data_t apply_actions(typename TreeState::action_t action0, typename TreeState::action_t action1)
     {
-        if (--this->depth_bound == 0)
-        {
-            this->payoff0 = this->device.random_int(2);
-            this->payoff1 = 1.0 - this->payoff0;
-            this->rows = 0;
-            this->cols = 0;
-        }
         typename TreeState::transition_data_t transition_data(0, Rational(1));
+        this->current = this->current->access(action0, action1)->access(transition_data);
+        this->payoff0 = this->current->stats.payoff;
+        this->payoff1 = 1 - this->payoff0;
         return transition_data;
     }
 };
