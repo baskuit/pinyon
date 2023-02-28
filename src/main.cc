1615e0a36eb771254e0bdcc75233df26ef7b9a0c
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-02-28 15:35:00 -0700

    pennymatching main

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,13 @@
 #include "state/test_states.hh"
+#include <iostream>
 
 int main()
 {
+
+    PennyMatching game;
+
+    game.apply_actions(0, 0);
+    std::cout << game.row_payoff << ' ' << game.col_payoff << std::endl;
+
     return 0;
 }
