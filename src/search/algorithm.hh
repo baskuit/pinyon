dc75de2924fa70bd64ce12c183a40afd52a5410d
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-03-04 22:20:40 -0700

    'BaseTreeBandit'. idk still cant change Outcome

--- src/search/algorithm.hh
+++ src/search/algorithm.hh
@@ -27,13 +27,13 @@ public:
 };
 
 template <class Model, class Algorithm>
-class TreeBandit : public AbstractAlgorithm<Model>
+class TreeBanditBase : public AbstractAlgorithm<Model>
 {
 public:
     struct Outcome;
     struct Types : AbstractAlgorithm<Model>::Types
     {
-        using Outcome = TreeBandit::Outcome;
+        using Outcome = TreeBanditBase::Outcome;
     };
 
     struct Outcome
@@ -56,46 +56,15 @@ public:
         }
     }
 
-private:
-
     MatrixNode<Algorithm> *playout(
         typename Types::State &state,
         typename Types::Model &model,
         MatrixNode<Algorithm> *matrix_node)
     {
-        if (!matrix_node->is_terminal)
-        {
-            if (matrix_node->is_expanded)
-            {
-                Outcome outcome;
-
-                this->select(matrix_node, outcome);
-
-                typename Types::Action row_action = matrix_node->actions.row_actions[outcome.row_idx];
-                typename Types::Action col_action = matrix_node->actions.col_actions[outcome.col_idx];
-                state.apply_actions(row_action, col_action);
-
-                ChanceNode<Algorithm> *chance_node = matrix_node->access(outcome.row_idx, outcome.col_idx);
-                MatrixNode<Algorithm> *matrix_node_next = chance_node->access(state.transition);
-
-                MatrixNode<Algorithm> *matrix_node_leaf = playout(state, model, matrix_node_next);
-
-                outcome.row_value = matrix_node_leaf->inference.row_value;
-                outcome.col_value = matrix_node_leaf->inference.col_value;
-                this->update_matrix_node(matrix_node, outcome);
-                this->update_chance_node(chance_node, outcome);
-                return matrix_node_leaf;
-            }
-            else
-            {
-                this->expand(state, model, matrix_node);
-                return matrix_node;
-            }
-        }
-        else
-        {
-            return matrix_node;
-        }
+        static_cast<Algorithm *>(this)->_playout(
+            state,
+            model,
+            matrix_node);
     }
 
     void select(
@@ -146,7 +115,55 @@ private:
             chance_node,
             outcome);
     }
+};
 
+template <class Model, class Algorithm>
+class TreeBandit : public TreeBanditBase<Model, Algorithm>
+{
+public:
+    struct Types : TreeBanditBase<Model, Algorithm>::Types
+    {
+    };
+
+    MatrixNode<Algorithm> *_playout(
+        typename Types::State &state,
+        typename Types::Model &model,
+        MatrixNode<Algorithm> *matrix_node)
+    {
+        if (!matrix_node->is_terminal)
+        {
+            if (matrix_node->is_expanded)
+            {
+                typename Types::Outcome outcome;
+
+                this->select(matrix_node, outcome);
+
+                typename Types::Action row_action = matrix_node->actions.row_actions[outcome.row_idx];
+                typename Types::Action col_action = matrix_node->actions.col_actions[outcome.col_idx];
+                state.apply_actions(row_action, col_action);
+
+                ChanceNode<Algorithm> *chance_node = matrix_node->access(outcome.row_idx, outcome.col_idx);
+                MatrixNode<Algorithm> *matrix_node_next = chance_node->access(state.transition);
+
+                MatrixNode<Algorithm> *matrix_node_leaf = this->_playout(state, model, matrix_node_next);
+
+                outcome.row_value = matrix_node_leaf->inference.row_value;
+                outcome.col_value = matrix_node_leaf->inference.col_value;
+                this->update_matrix_node(matrix_node, outcome);
+                this->update_chance_node(chance_node, outcome);
+                return matrix_node_leaf;
+            }
+            else
+            {
+                this->expand(state, model, matrix_node);
+                return matrix_node;
+            }
+        }
+        else
+        {
+            return matrix_node;
+        }
+    }
 };
 
 /*
@@ -154,22 +171,13 @@ TreeBandit with mutex's in each matrix node
 */
 
 template <class Model, class Algorithm>
-class TreeBanditThreaded : public AbstractAlgorithm<Model>
+class TreeBanditThreaded : public TreeBanditBase<Model, Algorithm>
 {
 public:
-    struct Outcome;
     struct MatrixStats;
-    struct Types : AbstractAlgorithm<Model>::Types
+    struct Types : TreeBanditBase<Model, Algorithm>::Types
     {
-        using Outcome = TreeBanditThreaded::Outcome;
-        using MatrixNode = TreeBanditThreaded::MatrixStats;
-    };
-
-    struct Outcome
-    {
-        int row_idx, col_idx;
-        typename Types::Real row_value, col_value;
-        typename Types::Real row_mu, col_mu; // Actor policy at row_idx, col_idx.
+        using MatrixStats = TreeBanditThreaded::MatrixStats;
     };
 
     struct MatrixStats
@@ -177,22 +185,7 @@ public:
         std::mutex mtx;
     };
 
-    void run(
-        int playouts,
-        typename Types::State &state,
-        typename Types::Model &model,
-        MatrixNode<Algorithm> &matrix_node)
-    {
-        init_stats(playouts, state, model, &matrix_node);
-        for (int playout = 0; playout < playouts; ++playout)
-        {
-            this->playout(state, model, &matrix_node);
-        }
-    }
-
-private:
-
-    MatrixNode<Algorithm> *playout(
+    MatrixNode<Algorithm> *_playout(
         typename Types::State &state,
         typename Types::Model &model,
         MatrixNode<Algorithm> *matrix_node)
@@ -204,7 +197,7 @@ private:
         {
             if (matrix_node->is_expanded)
             {
-                Outcome outcome;
+                typename Types::Outcome outcome;
 
                 mtx.lock();
                 this->select(matrix_node, outcome);
@@ -217,7 +210,7 @@ private:
                 ChanceNode<Algorithm> *chance_node = matrix_node->access(outcome.row_idx, outcome.col_idx);
                 MatrixNode<Algorithm> *matrix_node_next = chance_node->access(state.transition);
 
-                MatrixNode<Algorithm> *matrix_node_leaf = playout(state, model, matrix_node_next);
+                MatrixNode<Algorithm> *matrix_node_leaf = this->_playout(state, model, matrix_node_next);
 
                 outcome.row_value = matrix_node_leaf->inference.row_value;
                 outcome.col_value = matrix_node_leaf->inference.col_value;
@@ -240,54 +233,4 @@ private:
             return matrix_node;
         }
     }
-
-    void select(
-        MatrixNode<Algorithm> *matrix_node,
-        Outcome &outcome)
-    {
-        static_cast<Algorithm *>(this)->_select(
-            matrix_node,
-            outcome);
-    }
-
-    void init_stats(
-        int playouts,
-        typename Types::State &state,
-        typename Types::Model &model,
-        MatrixNode<Algorithm> *root)
-    {
-        static_cast<Algorithm *>(this)->_init_stats(
-            playouts,
-            state,
-            model,
-            root);
-    }
-    void expand(
-        typename Types::State &state,
-        typename Types::Model &model,
-        MatrixNode<Algorithm> *matrix_node)
-    {
-        static_cast<Algorithm *>(this)->_expand(
-            state,
-            model,
-            matrix_node);
-    }
-    void update_matrix_node(
-        MatrixNode<Algorithm> *matrix_node,
-        Outcome &outcome)
-    {
-        static_cast<Algorithm *>(this)->_update_matrix_node(
-            matrix_node,
-            outcome);
-    }
-
-    void update_chance_node(
-        ChanceNode<Algorithm> *chance_node,
-        Outcome &outcome)
-    {
-        static_cast<Algorithm *>(this)->_update_chance_node(
-            chance_node,
-            outcome);
-    }
-
 };
