eb7d8f8fe9621b6eb06030686dc21f64e9b421e4
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-03-22 21:47:21 -0700

    LibGambit to remove boilerplate solve

--- random_trees/grow.hh
+++ random_trees/grow.hh
@@ -2,10 +2,9 @@
 
 #include <string>
 
+#include "libsurskit/gambit.hh"
 #include "tree/tree.hh"
 #include "algorithm/algorithm.hh"
-
-#include "solvers/enummixed/enummixed.h"
 
 template <typename Model>
 class Grow : public AbstractAlgorithm<Model>
@@ -28,11 +27,7 @@ public:
         int count = 1;
     };
 
-    using Solver = Gambit::Nash::EnumMixedStrategySolver<double>;
-    using Solution = Gambit::List<Gambit::List<Gambit::MixedStrategyProfile<double>>>;
-
     prng &device;
-    Solver solver;
     bool require_interior = false;
 
     Grow(prng &device) : device(device)
@@ -81,7 +76,7 @@ public:
         }
         else
         {
-            solve_matrix(
+            LibGambit::solve_matrix<Types>(
                 matrix_node->stats.expected_value,
                 matrix_node->stats.row_strategy,
                 matrix_node->stats.col_strategy);
@@ -97,50 +92,4 @@ public:
             }
         }
     }
-
-private:
-    void solve_matrix(
-        typename Types::MatrixReal &matrix,
-        typename Types::VectorReal &row_strategy,
-        typename Types::VectorReal &col_strategy)
-    {
-        Gambit::Game game = build_nfg(matrix);
-        Gambit::shared_ptr<Gambit::Nash::EnumMixedStrategySolution<double>> solution = this->solver.SolveDetailed(game);
-        Solution cliques = solution->GetCliques();
-        Gambit::MixedStrategyProfile<double> joint_strategy = cliques[1][1];
-        double is_interior = 1.0;
-        for (int i = 0; i < matrix.rows; ++i)
-        {
-            row_strategy[i] = joint_strategy[i + 1];
-            is_interior *= 1 - row_strategy[i];
-        }
-        for (int j = matrix.rows; j < matrix.rows + matrix.cols; ++j)
-        {
-            col_strategy[j - matrix.rows] = joint_strategy[j + 1];
-            is_interior *= 1 - col_strategy[j];
-        }
-        delete game;
-    }
-
-        Gambit::Game build_nfg(
-            typename Types::MatrixReal &matrix)
-        {
-            Gambit::Array<int> dim(2);
-            dim[1] = matrix.rows;
-            dim[2] = matrix.cols;
-            Gambit::GameRep *nfg = NewTable(dim);
-            Gambit::Game game = nfg;
-            Gambit::StrategyProfileIterator iter(Gambit::StrategySupportProfile(static_cast<Gambit::GameRep *>(nfg)));
-            for (int j = 0; j < matrix.cols; ++j)
-            {
-                for (int i = 0; i < matrix.rows; ++i)
-                {   
-                    const typename Types::Real x = matrix.data[i][j];
-                    (*iter)->GetOutcome()->SetPayoff(1, std::to_string(x));
-                    (*iter)->GetOutcome()->SetPayoff(2, std::to_string(1 - x));
-                    iter++;
-                }
-            }
-            return game;
-        }
 };
