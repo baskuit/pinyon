4352d6b57c61158d41fc1bd9276606dd150645a8
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-03-26 07:59:59 -0700

    matrixucb thread test

--- test/thread_test.cc
+++ test/thread_test.cc
@@ -12,20 +12,22 @@ int main()
     using MoldState = MoldState<size>;
     using Model = MonteCarloModel<MoldState>;
     using Exp3p = Exp3p<Model, TreeBanditThreaded>;
+    using MatrixUCB = MatrixUCB<Model, TreeBanditThreaded>;
 
-    MoldState game(20);
+    const int depth = 10;
+    MoldState game(depth);
     prng device(0);
     Model model(device);
-    MatrixNode<Exp3p> root;
-    Exp3p session(device);
-    session.threads = 8;
-    const int playouts = 10000000;
+    MatrixNode<MatrixUCB> root;
+    MatrixUCB session(device);
+    session.threads = 1;
+    const int playouts = 1000000;
 
-    std::cout << "Threaded speed test with threads=" << session.threads << ", playouts = " << playouts << std::endl;
+    std::cout << "Threaded speed test with threads=" << session.threads << ", playouts = " << playouts << ", depth = " << depth << std::endl;
 
     session.run(playouts, device, game, model, root);
 
-    typename Exp3p::Types::VectorReal row_strategy, col_strategy;
+    typename Model::Types::VectorReal row_strategy, col_strategy;
     session.get_strategies(&root, row_strategy, col_strategy);
     math::print(row_strategy, size);
     math::print(col_strategy, size);
