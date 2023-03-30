dbb8bca37c329c643e9a9ad1fc8df5b631a46c69
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-03-30 06:05:20 -0700

    remove double, notice impl error!!!

--- src/algorithm/exp3p.hh
+++ src/algorithm/exp3p.hh
@@ -8,12 +8,7 @@
 Exp3p
 */
 
-/*
-NOTE: I'm not sure how Exp3p is supposed to be thread safe if the one session (Algorithm) has only one helper forecast Vector.
-*/
-
-
-template <class Model, template <class _Model, class _BanditAlgorithm_> class _TreeBandit>
+template <class Model, template <class _Model, class _BanditAlgorithm> class _TreeBandit>
 class Exp3p : public _TreeBandit<Model, Exp3p<Model, _TreeBandit>>
 {
 public:
@@ -33,15 +28,15 @@ public:
         typename Types::VectorInt col_visits;
 
         int visits = 0;
-        double row_value_total = 0;
-        double col_value_total = 0;
+        typename Types::Real row_value_total = 0;
+        typename Types::Real col_value_total = 0;
     };
 
     struct ChanceStats : _TreeBandit<Model, Exp3p<Model, _TreeBandit>>::ChanceStats
     {
         int visits = 0;
-        double row_value_total = 0;
-        double col_value_total = 0;
+        typename Types::Real row_value_total = 0;
+        typename Types::Real col_value_total = 0;
     };
 
     Exp3p () {}
@@ -109,11 +104,11 @@ public:
             MatrixNode<Exp3p> *matrix_parent = chance_parent->parent;
             int row_idx = chance_parent->row_idx;
             int col_idx = chance_parent->col_idx;
-            double reach_probability = // double to mix Real and Probabilty type..., p much all use cases should have conversion tho
+            typename Types::Real reach_probability =
                 matrix_parent->inference.row_policy[row_idx] *
                 matrix_parent->inference.col_policy[col_idx] *
-                ((double)matrix_node->transition.prob);
-            int time_estimate = matrix_parent->stats.time * reach_probability;
+                ((typename Types::Real)matrix_node->transition.prob);
+            int time_estimate = matrix_parent->stats.time * reach_probability; // TODO 
             time_estimate = time_estimate == 0 ? 1 : time_estimate;
             matrix_node->stats.time = time_estimate;
         }
@@ -139,15 +134,15 @@ public:
         }
         else
         {
-            const double eta = .95 * sqrt(log(rows) / (time * rows));
-            const double gamma_ = 1.05 * sqrt(rows * log(rows) / time);
-            const double gamma = gamma_ < 1 ? gamma_ : 1;
+            const typename Types::Real eta = .95 * sqrt(log(rows) / (time * rows));
+            const typename Types::Real gamma_ = 1.05 * sqrt(rows * log(rows) / time);
+            const typename Types::Real gamma = gamma_ < 1 ? gamma_ : 1;
             softmax(row_forecast, matrix_node->stats.row_gains, rows, eta);
             for (int row_idx = 0; row_idx < rows; ++row_idx)
             {
-                row_forecast[row_idx] = // conversion from double to Real?, TODO TODO
+                row_forecast[row_idx] =
                     (1 - gamma) * row_forecast[row_idx] +
-                    (gamma)*matrix_node->inference.row_policy[row_idx];
+                    (gamma) * matrix_node->inference.row_policy[row_idx];
             }
         }
         if (cols == 1)
@@ -156,15 +151,15 @@ public:
         }
         else
         {
-            const double eta = .95 * sqrt(log(cols) / (time * cols));
-            const double gamma_ = 1.05 * sqrt(cols * log(cols) / time);
-            const double gamma = gamma_ < 1 ? gamma_ : 1;
+            const typename Types::Real eta = .95 * sqrt(log(cols) / (time * cols));
+            const typename Types::Real gamma_ = 1.05 * sqrt(cols * log(cols) / time);
+            const typename Types::Real gamma = gamma_ < 1 ? gamma_ : 1;
             softmax(col_forecast, matrix_node->stats.col_gains, cols, eta);
             for (int col_idx = 0; col_idx < cols; ++col_idx)
             {
                 col_forecast[col_idx] =
                     (1 - gamma) * col_forecast[col_idx] +
-                    (gamma)*matrix_node->inference.col_policy[col_idx];
+                    (gamma) * matrix_node->inference.col_policy[col_idx];
             }
         }
         const int row_idx = device.sample_pdf(row_forecast, rows);
@@ -184,14 +179,18 @@ public:
         matrix_node->stats.visits += 1;
         matrix_node->stats.row_visits[outcome.row_idx] += 1;
         matrix_node->stats.col_visits[outcome.col_idx] += 1;
-        const double rows = matrix_node->actions.rows;
-        const double cols = matrix_node->actions.cols;
-        const double time = matrix_node->stats.time;
-        const double row_beta = std::sqrt(std::log(rows) / (double)(time * rows));
-        const double col_beta = std::sqrt(std::log(cols) / (double)(time * cols));
-        matrix_node->stats.row_gains[outcome.row_idx] += outcome.row_value / outcome.row_mu + row_beta; // TODO check this lmao
+        const typename Types::Real rows = matrix_node->actions.rows;
+        const typename Types::Real cols = matrix_node->actions.cols;
+        const typename Types::Real time = matrix_node->stats.time;
+        const typename Types::Real row_beta = std::sqrt(std::log(rows) / (typename Types::Real)(time * rows));
+        const typename Types::Real col_beta = std::sqrt(std::log(cols) / (typename Types::Real)(time * cols));
+        matrix_node->stats.row_gains[outcome.row_idx] += outcome.row_value / outcome.row_mu + row_beta; //TODO TODO TODO this is INCORRECT
         matrix_node->stats.col_gains[outcome.col_idx] += outcome.col_value / outcome.col_mu + col_beta;
     }
+
+    /*
+    Fixing this requires keeping the entire forecast, so will have to change outcome or refactor to add outcome as a template param or smth
+    */
 
     void update_chance_node(
         ChanceNode<Exp3p> *chance_node,
@@ -210,23 +209,23 @@ private:
         typename Types::Real eta)
     {
         /*
-        Softmax helper function with logit scaling and uniform noise.
+        Softmax helper function with logit scaling.
         */
-        double max = 0;
+        typename Types::Real max = 0;
         for (int i = 0; i < k; ++i)
         {
-            double x = gains[i];
+            typename Types::Real x = gains[i];
             if (x > max)
             {
                 max = x;
             }
         }
-        double sum = 0;
+        typename Types::Real sum = 0;
         for (int i = 0; i < k; ++i)
         {
             gains[i] -= max;
-            double x = gains[i];
-            double y = std::exp(x * eta);
+            typename Types::Real x = gains[i];
+            typename Types::Real y = std::exp(x * eta);
             forecast[i] = y;
             sum += y;
         }
