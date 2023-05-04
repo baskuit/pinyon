bfa56403bcd6c71159fd21c038a6d7b0f953cfc9
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-04 05:56:46 -0700

    WIP generalizing TreeState

--- random_trees/tree-state.hh
+++ random_trees/tree-state.hh
@@ -6,75 +6,85 @@
 
 #include <memory>
 
-template <int size>
-class TreeState : public SolvedStateArray<size, int, int, double>
-{
-public:
-    using SeedStateNode = MatrixNode<Grow<MonteCarloModel<SeedState<size>>>>;
+/*
 
-    struct Types : SolvedStateArray<size, int, int, double>::Types
+This state is a wrapper for the tree produced by the Grow algorithm
+
+What should template type be? Just model?
+
+*/
+
+template <class Model>
+class TreeState : public SolvedState<Model::Types::TypeList>
+{
+    static_assert(std::derived_from<Model::Types::State, StateChance<Model::Types::TypeList>>,
+        "TreeState must be based on State type derived from StateChance");
+    static_assert(std::derived_from<Model, DoubleOracleModel>,
+        "Grow algorithm requires Model must be derived from DoubleOracleModel");
+
+public:
+    using Node = MatrixNode<Grow<Model>>;
+
+    struct Types : Model::Types
     {
     };
 
-    SeedState<size> seed;
-    std::shared_ptr<SeedStateNode> root;
-    SeedStateNode *current;
+    std::shared_ptr<Node> tree;
+    Node *current;
 
-    TreeState(prng &device, int depth_bound, int rows, int cols, int (*depth_bound_func)(prng &, int), int (*actions_func)(prng &, int)) : 
-        seed(SeedState<size>(device, depth_bound, rows, cols, depth_bound_func, actions_func))
+    TreeState(
+        typename Types::State &state,
+        Model &model,
+        int max_depth)
     {
-        this->root = std::make_shared<SeedStateNode>();
-        this->current = &*root;
-        Grow<MonteCarloModel<SeedState<size>>> session;
-        session.grow(device, seed, current);
-        update_solved_state_payoffs(current);
-        this->transition = seed.transition; // total hack
+        this->tree = std::make_shared<Node>();
+        this->current = &*tree;
+        Grow<Model> session;
+        session.max_depth = max_depth;
+        session.grow(state, model, current);
+        update_solved_state_info(current);
     }
 
     void get_actions()
     {
-        this->actions.rows = this->current->stats.expected_value.rows;
-        this->actions.cols = this->current->stats.expected_value.cols;
-        this->actions.row_actions.fill(this->actions.rows);
-        this->actions.col_actions.fill(this->actions.cols);
-        for (int i = 0; i < this->actions.rows; ++i)
-        {
-            this->actions.row_actions[i] = i;
-        };
-        for (int j = 0; j < this->actions.cols; ++j)
-        {
-            this->actions.col_actions[j] = j;
-        };
+        this->actions = current->actions;
     }
 
     void apply_actions(
         typename Types::Action row_action,
         typename Types::Action col_action)
     {
-        typename SeedState<size>::Transition transition;
-        transition.obs = 0;
-        transition.prob = 1;
-        this->current = this->current->access(row_action, col_action)->access(transition);
-        this->is_terminal = this->current->is_terminal; 
-        update_solved_state_payoffs(this->current);
+        this->current = this->current->access(row_action, col_action)->access(transition); // TODO how to make transition like the OG state without a state?
+        update_solved_state_info(this->current);
+    }
+
+    void apply_actions(
+        typename Types::Action row_action,
+        typename Types::Action col_action,
+        typename Types::Observation chance_action)
+    {
+        this->transition.obs = chance_action;
+        this->current = this->current->access(row_action, col_action)->access(this->transition);
+        update_solved_state_info(this->current);
     }
 
 private:
-    void update_solved_state_payoffs(SeedStateNode *current)
+    void update_solved_state_info(Node *current)
     {
-        this->row_payoff = this->current->stats.payoff;
-        this->col_payoff = 1 - this->row_payoff;
-        const int rows = this->current->actions.rows;
-        const int cols = this->current->actions.cols;
-        this->row_strategy.fill(rows);
-        this->col_strategy.fill(cols);
-        for (int i = 0; i < rows; ++i)
-        {
-            this->row_strategy[i] = this->current->stats.row_strategy[i];
-        }
-        for (int j = 0; j < cols; ++j)
-        {
-            this->col_strategy[j] = this->current->stats.col_strategy[j];
-        }
+        this->is_terminal = this->current->is_terminal; 
+        // this->row_payoff = this->current->stats.payoff;
+        // this->col_payoff = 1 - this->row_payoff;
+        // const int rows = this->current->actions.rows;
+        // const int cols = this->current->actions.cols;
+        // this->row_strategy.fill(rows);
+        // this->col_strategy.fill(cols);
+        // for (int i = 0; i < rows; ++i)
+        // {
+        //     this->row_strategy[i] = this->current->stats.row_strategy[i];
+        // }
+        // for (int j = 0; j < cols; ++j)
+        // {
+        //     this->col_strategy[j] = this->current->stats.col_strategy[j];
+        // }
     }
 };
