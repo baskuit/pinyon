749cee0a9346d2866013d3b3389a2012f6085309
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-04 00:41:25 -0700

    more alphabeta pseudocode

--- random_trees/alphabeta.hh
+++ random_trees/alphabeta.hh
@@ -2,6 +2,15 @@
 #include "tree/tree.hh"
 #include "algorithm/algorithm.hh"
 
+/*
+
+An implementation of Simultaneous Move Alpha Beta
+
+See "Using Double-Oracle Method and Serialized Alpha-Beta Search
+for Pruning in Simultaneous Move Games"
+
+*/
+
 template <typename Model>
 class AlphaBeta : public AbstractAlgorithm<Model>
 {
@@ -19,7 +28,7 @@ class AlphaBeta : public AbstractAlgorithm<Model>
         typename Types::MatrixReal o;
         typename Types::Real alpha = min_val;
         typename Types::Real beta = max_val;
-        typename Types::VectorInt I, J;
+        typename Types::VectorInt I, J; // use raw indices
     } struct ChanceStats
     {
         typename Types::Probability explored = 0;
@@ -31,15 +40,58 @@ class AlphaBeta : public AbstractAlgorithm<Model>
     AlphaBeta(typename Types::Real min_val, typename Types::Real max_val) : min_val(min_val), max_val(max_val) {}
 
     typename Types::Real double_oracle(
-        typename Types::State state,
+        typename Types::State &state,
+        MatrixNode<AlphaBeta> *matrix_node,
         typename Types::Real alpha,
         typename Types::Real beta)
     {
 
+        typename Types::MatrixReal &p = matrix_node->stats.p;
+        typename Types::MatrixReal &q = matrix_node->stats.q;
+
+
+        // double oracle is only called on a state exactly once, so we can expand here
+        state.get_actions()
+        matrix_node.actions = state.actions;
+
+        p;
+        // set to minval
+        o;
+        // set to maxval
+
+        // Solving a restricted matrix is done by creating a new one? It's basically only done one and then the matrix is expanded
+
+
+
         if (state.is_terminal) {
             return state.row_payoff;
         }
+
+        while (alpha < beta) { // consider fuzzy eq
+
+            for (int i : matrix_node.stats.I) {
+                for (int j : matrix_node.stats.J) {
+                    auto p_ij = p.get(i, j);
+                    auto o_ij = o.get(i, j);
+
+                    if (p_ij < o_ij) {
+                        auto u_ij = double_oracle(s_ij, matrix_node_next p_ij, o_ij)
+                        p_ij = o_ij = u_ij;
+                    }
+
+                }
+            }
+
+            // get restricted matrix of u values
+            typename Types::MatrixReal M;
+
+            // Calculate NE on restricted stage game
+            typename Types::VectorReal row_strategy, col_strategy;
+            Gambit::SolveMatrix(M, row_strategy, col_strategy);
+
+            
         
+        }
 
         return .5;
     }
