a3de7b262722aa7f9b6910e3a4883947ca56bc2c
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-08 11:04:09 -0700

    update test

--- random-trees/test.cc
+++ random-trees/test.cc
@@ -29,46 +29,37 @@ int main()
 {
 
     const int MaxActions = 3;
-    const int MaxTransitions = 1;
+    const int MaxTransitions = 2;
 
     using SeedState = SeedState<MaxActions, MaxTransitions>;
     using Model = MonteCarloModel<SeedState>;
     using MatrixUCB = MatrixUCB<Model, TreeBandit>;
+    using Exp3p = Exp3p<Model, TreeBandit>;
+    using Algorithm = MatrixUCB;
 
-    prng device;
+    prng init_device;
+    int seed = 76874;//init_device.random_int(100000);
+    prng device(seed);
 
-    SeedState state(device, 1, 3, 3, nullptr, nullptr, nullptr);
+    SeedState state(device, 2, 3, 3, nullptr, nullptr, nullptr);
     Model model(device);
-    MatrixUCB session;
-    MatrixNode<MatrixUCB> root;
 
-    session.run(10, device, state, model, root);
+    // Algorithm session;
+    // MatrixNode<Algorithm> root;
+    // session.run(1000, device, state, model, root);
 
-    MatrixUCB::Types::VectorReal row_strategy(3), col_strategy(3);
-    MatrixUCB::Types::MatrixReal row_value_matrix(3, 3), col_value_matrix(3, 3);
+    // SeedState::Types::MatrixReal row_value_matrix(3, 3), col_value_matrix(3, 3);
+    // session.get_ev_matrix(&root, row_value_matrix, col_value_matrix);
+    // std::cout << "expected value matrix:\n";
+    // row_value_matrix.print();
 
-    session.get_ev_matrix(&root, row_value_matrix, col_value_matrix);
+    // SeedState::Types::VectorReal row_strategy(3), col_strategy(3);
+    // session.get_strategies(&root, row_strategy, col_strategy);
+    // std::cout << "strategies:\n";
+    // math::print(row_strategy, 3);
+    // math::print(col_strategy, 3);
 
-    std::cout << "row ev matrix:\n";
-    row_value_matrix.print();
-    print_matrix(&root);
-    session.get_strategies(&root, row_strategy, col_strategy);
-    std::cout << "strategies:\n";
-    math::print(row_strategy, 3);
-    math::print(col_strategy, 3);
-
-
-
-    // double expl = Linear::exploitability<SeedState::Types::TypeList>(
-    //     row_payoff_matrix, 
-    //     col_payoff_matrix, 
-    //     row_strategy, 
-    //     col_strategy);
-    // std::cout << "session expl: " << expl << '\n';
     /*
-    Bug cus no call get_actions to get transition_strategies
-    Where should we put the get_actions call to guarantee no search is done on state that doesnt have actions?
-    If put at run, then
     */
 
     Grow<Model> grow;
@@ -80,12 +71,27 @@ int main()
 
     std::cout << "node count: " << grow_root.stats.matrix_node_count << '\n';
     std::cout << "row payoff matrix:\n";
-    MatrixUCB::Types::MatrixReal row_payoff_matrix = grow_root.stats.nash_payoff_matrix;
-    MatrixUCB::Types::MatrixReal col_payoff_matrix = row_payoff_matrix * -1 + 1;
+    SeedState::Types::MatrixReal row_payoff_matrix = grow_root.stats.nash_payoff_matrix;
+    SeedState::Types::MatrixReal col_payoff_matrix = row_payoff_matrix * -1 + 1;
     row_payoff_matrix.print();
     std::cout << "solutions:\n";
     math::print(row_solution, 3);
     math::print(col_solution, 3);
+
+    
+
+    // double expl = Linear::exploitability<SeedState::Types::TypeList>(
+    //     row_payoff_matrix, 
+    //     col_payoff_matrix, 
+    //     row_strategy, 
+    //     col_strategy);
+    std::cout << "session expl: " << expl << '\n';
+
+    for (auto c :state.transition_strategies) {
+        std::cout << c << ' ';
+    }
+    std::cout << std::endl;
+    std::cout << "seed: " << seed << std::endl;
 
     
 
