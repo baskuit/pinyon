970b4e5e3bfa9b7f4da7272fcc80cdf620e49768
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-08 00:35:15 -0700

    add untested gambit conversion for rational

--- src/libsurskit/rational.hh
+++ src/libsurskit/rational.hh
@@ -32,6 +32,44 @@ public:
 
     Rational(int p, int q) : p(p), q(q) {}
 
+    Rational(double x)
+    {
+        p = 0;
+        q = 1;
+        if (x != 0.0) {
+            bool neg = x < 0;
+            if (neg) {
+                x = -x;
+            }
+
+            constexpr long shift = 15;         // a safe shift per step
+            constexpr double width = 1 << shift;  // = 2^shift
+            const int maxiter = 20;        // ought not be necessary, but just in case,
+            // max 300 bits of precision
+            int expt;
+            double mantissa = frexp(x, &expt);
+            long exponent = expt;
+            double intpart;
+            int k = 0;
+            while (mantissa != 0.0 && k++ < maxiter)
+            {
+                mantissa *= width;
+                mantissa = modf(mantissa, &intpart);
+                p <<= shift;
+                p += (long)intpart;
+                exponent -= shift;
+            }
+            if (exponent > 0)
+                p <<= exponent;
+            else if (exponent < 0)
+                q <<= -exponent;
+            if (neg) {
+                p *= -1;
+            }
+        }
+        reduce();
+    }
+
     Rational operator+(Rational y)
     {
         Rational z = {p * y.q + y.p * q, q * y.q};
