55ee986d87ec87ee1e882161301fb8f315d57d39
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-11 23:36:23 -0700

    col_strategy becomes J, array size issue forgot to fill strategies when solving

--- random-trees/test.cc
+++ random-trees/test.cc
@@ -31,8 +31,7 @@ int main()
 {
 
     const int MaxActions = 4;
-    const int MaxTransitions = 2;
-    const int depth_bound = 1;
+    const int MaxTransitions = 1;
     const double chance_threshold = 1 / (double) 3; 
 
     using SeedState = SeedState<MaxActions, MaxTransitions>;
@@ -45,69 +44,33 @@ int main()
     SeedState::Types::MatrixReal row_value_matrix(MaxActions, MaxActions), col_value_matrix(MaxActions, MaxActions);
 
     prng init_device;
-    int seed = 0;//init_device.uniform_64();
+    int seed = 100;//init_device.uniform_64();
     std::cout << "seed: " << seed << std::endl;
     prng device(seed);
-    int games = 20;
-
-    for (int iterations = 100; iterations <= 1000000; iterations *= 10) {
-        for (int actions = 2; actions <= MaxActions; ++actions) {
-            for (int depth_bound = 1; depth_bound < 4; ++depth_bound) {
+    int games = 10;
 
 
-                double total_exp3p_expl = 0;
-                double total_matrix_ucb_expl = 0;
-
-                for (int game = 0; game < games; ++game) {
-
-                    SeedState state(device, depth_bound, MaxActions, MaxActions, chance_threshold);
-                    Model model(device);
-                    TreeState<Model> tree_state(state, model);
-
-                    AlphaBeta<Model> alpha_beta_session(0, 1);
-                    alpha_beta_session.run(state, model);
+    for (int actions = 2; actions <= MaxActions; ++actions) {
+        for (int depth_bound = 1; depth_bound < 2; ++depth_bound) {
 
 
-                //     SeedState::Types::MatrixReal row_payoff_matrix;
-                //     SeedState::Types::MatrixReal col_payoff_matrix;
-                //     tree_state.get_payoff_matrix(row_payoff_matrix, col_payoff_matrix);
+            double total_exp3p_expl = 0;
+            double total_matrix_ucb_expl = 0;
 
+            for (int game = 0; game < games; ++game) {
 
-                //     Exp3p exp3p_session;
-                //     MatrixNode<Exp3p> exp3p_root;
-                //     exp3p_session.run(iterations, device, state, model, exp3p_root);
-                //     exp3p_session.get_ev_matrix(&exp3p_root, row_value_matrix, col_value_matrix);
-                //     exp3p_session.get_strategies(&exp3p_root, row_strategy, col_strategy);
-                //     double exp3p_expl = Linear::exploitability<SeedState::Types::TypeList>(
-                //         row_payoff_matrix, 
-                //         col_payoff_matrix, 
-                //         row_strategy, 
-                //         col_strategy);
+                SeedState state(device, depth_bound, actions, actions, chance_threshold);
+                Model model(device);
+                TreeState<Model> tree_state(state, model);
 
-
-
-                //     MatrixUCB matrix_ucb_session;
-                //     MatrixNode<MatrixUCB> matrix_ucb_root;
-                //     matrix_ucb_session.run(iterations, device, state, model, matrix_ucb_root);
-                //     matrix_ucb_session.get_ev_matrix(&matrix_ucb_root, row_value_matrix, col_value_matrix);
-                //     matrix_ucb_session.get_strategies(&matrix_ucb_root, row_strategy, col_strategy);
-                //     double matrix_ucb_expl = Linear::exploitability<SeedState::Types::TypeList>(
-                //         row_payoff_matrix, 
-                //         col_payoff_matrix, 
-                //         row_strategy, 
-                //         col_strategy);
-
-                //     total_exp3p_expl += exp3p_expl;
-                //     total_matrix_ucb_expl += matrix_ucb_expl;
-                // }
-
-                // std::cout << "iter: " << iterations << " actions: " << actions << " depth_bound: " << depth_bound << std::endl;
-                // std::cout << "mean expl exp3p: " << total_exp3p_expl / games << std::endl;
-                // std::cout << "mean expl matrix_ucb: " << total_matrix_ucb_expl / games << std::endl;
-                }
-
+                AlphaBeta<Model> alpha_beta_session(0, 1);
+                alpha_beta_session.run(state, model);
+                std::cout << "TreeState value: " << tree_state.current_node->stats.row_payoff << '\n';
+                return 0;
             }
+
         }
     }
+
     return 0;
 }
