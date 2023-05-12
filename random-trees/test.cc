edac8ade89468a3a8d3ef8408cb18da0a9f87810
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-12 13:02:40 -0700

    working test

--- random-trees/test.cc
+++ random-trees/test.cc
@@ -27,55 +27,52 @@ void print_matrix (MatrixNode<Algorithm> *matrix_node) {
     }
 }
 
-int main()
+template <size_t MaxActions, size_t MaxTransitions>
+void alpha_beta_test (
+    const int min_depth, const int max_depth,
+    const int games, const uint64_t seed = 0) 
 {
-
-    const int MaxActions = 4;
-    const int MaxTransitions = 1;
-    const double chance_threshold = 1 / (double) 3; 
-
     using SeedState = SeedState<MaxActions, MaxTransitions>;
     using Model = MonteCarloModel<SeedState>;
     using MatrixUCB = MatrixUCB<Model, TreeBandit>;
     using Exp3p = Exp3p<Model, TreeBandit>;
     using Algorithm = Exp3p;
 
-    SeedState::Types::VectorReal row_strategy(MaxActions), col_strategy(MaxActions);
-    SeedState::Types::MatrixReal row_value_matrix(MaxActions, MaxActions), col_value_matrix(MaxActions, MaxActions);
-
-    prng init_device;
-    int seed = 100;//init_device.uniform_64();
-    // int seed = init_device.uniform_64();
-
-    std::cout << "seed: " << seed << std::endl;
     prng device(seed);
-    int games = 1;
+    Rational chance_threshold = Rational(0);
 
-
-    for (int actions = 4; actions <= MaxActions; ++actions) {
-        for (int depth_bound = 2; depth_bound < 3; ++depth_bound) {
+    for (int actions = 2; actions <= MaxActions; ++actions) {
+        for (int depth_bound = min_depth; depth_bound <= max_depth; ++depth_bound) {
 
             double total_exp3p_expl = 0;
             double total_matrix_ucb_expl = 0;
 
             for (int game = 0; game < games; ++game) {
-                // const uint64_t seed = device.uniform_64(); //10641954317717599816;
-                const uint64_t seed = 10641954317717599816;
-                prng new_device(seed);
-                std::cout << seed << std::endl;
+                const uint64_t new_seed = device.uniform_64();
+                // const uint64_t seed = 10641954317717599816;
+                prng new_device(new_seed);
                 SeedState state(new_device, depth_bound, actions, actions, chance_threshold);
                 Model model(new_device);
                 TreeState<Model> tree_state(state, model);
-                std::cout << actions << depth_bound << std::endl;
-                std::cout << "TreeState value: " << tree_state.current_node->stats.row_payoff << '\n';
                 AlphaBeta<Model> alpha_beta_session(0, 1);
-                alpha_beta_session.run(state, model, tree_state.current_node);
-                
+                MatrixNode<AlphaBeta<Model>> alpha_beta_root;
+                alpha_beta_session.run(state, model, &alpha_beta_root, tree_state.current_node);
+                auto a = alpha_beta_root.stats.value;
+                auto b = tree_state.current_node->stats.row_payoff;
+                std::cout << a << ' ' << b << std::endl;
+                // assert(alpha_beta_session.equals(a, b));
 
             }
 
         }
     }
+
+}
+
+int main()
+{
+
+    alpha_beta_test<5, 1>(1, 5, 100);
 
     return 0;
 }
