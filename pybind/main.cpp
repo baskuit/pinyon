2fda2d1f5da8939bf41ed908ec7e142d55c8bdbf
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-05-26 05:34:24 -0700

    some pybind classes

--- pybind/main.cpp
+++ pybind/main.cpp
@@ -1,18 +1,14 @@
 #include <pybind11/pybind11.h>
 
 #include <surskit.hh>
-// #include "../src/surskit.hh"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(pysurksit, m) {
+PYBIND11_MODULE(pysurskit, m) {
+
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -26,7 +22,24 @@ PYBIND11_MODULE(pysurksit, m) {
            subtract
     )pbdoc";
 
-    py::class_<RandomTree>(m, "RandomTree");
+    py::class_<EmptyClass>(m, "EmptyClass").def(py::init<>());
+
+    py::class_<prng>(m, "prng").def(py::init<>()).def(py::init<int>());
+
+    py::class_<RandomTree>(m, "RandomTree").def(py::init<prng, int, int, int, int, double>())
+        .def("__dir__", [](const RandomTree& obj) {
+            py::list result;
+            py::handle scope = py::getattr(py::module::import("__main__"), "__dict__");
+            auto class_dict = py::getattr(scope, "RandomTree");
+            py::dict dict(class_dict);
+            for (auto item : dict) {
+                if (py::isinstance<py::function>(item.second)) {
+                    result.append(item.first);
+                }
+            }
+            return result;
+        });
+    
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
