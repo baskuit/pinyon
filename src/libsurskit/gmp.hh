91dea55454e7a2397c32555548f0e24ed21c61ab
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-06-23 12:03:57 -0700

    fix

--- src/libsurskit/gmp.hh
+++ src/libsurskit/gmp.hh
@@ -7,66 +7,6 @@
 namespace LRSNash
 {
 
-    template <class Types>
-    void solve_matrix(
-        typename Types::MatrixValue &payoff_matrix,
-        typename Types::VectorReal &row_strategy,
-        typename Types::VectorReal &col_strategy,
-        const size_t n_discrete = 20)
-    {
-        const int rows = payoff_matrix.rows;
-        const int cols = payoff_matrix.cols;
-        row_strategy.fill(rows);
-        col_strategy.fill(cols);
-
-        const auto min = payoff_matrix.min();
-        const auto max = payoff_matrix.max();
-        auto delta = (max - min);
-
-        Matrix<PairDouble> normalized{rows, cols};
-        int idx = 0;
-        for (const auto value : payoff_matrix)
-        {
-            normalized[idx].row_value = static_cast<double>((value.get_row_value() - min) / delta);
-            normalized[idx].col_value = static_cast<double>((value.get_col_value() - min) / delta);
-            ++idx;
-        }
-
-        std::array<int, 18> row_num, row_den{n_discrete}, col_num, col_den{n_discrete};
-
-        idx = 0;
-        for (const auto value : normalized)
-        {
-            row_num[idx] = floor(value.get_row_value() * n_discrete);
-            col_num[idx] = floor(value.get_col_value() * n_discrete);
-            ++idx;
-        }
-
-        game g;
-        init_game(&g, rows, cols, row_num.data(), row_den.data(), col_num.data(), col_den.data());
-
-        auto row_data = alloc_data(rows + 2);
-        auto col_data = alloc_data(cols + 2);
-
-        solve(&g, row_data, col_data);
-
-        double x{1 / mpz_get_d(row_data[0])};
-        for (int row_idx = 0; row_idx < rows; ++row_idx)
-        {
-            row_strategy[row_idx] = mpz_get_d(row_data[row_idx + 1]) * x;
-        }
-
-        double y{1 / mpz_get_d(col_data[0])};
-
-        for (int col_idx = 0; col_idx < cols; ++col_idx)
-        {
-            col_strategy[col_idx] = mpz_get_d(col_data[col_idx + 1]) * y;
-        }
-
-        dealloc_data(row_data, rows + 2);
-        dealloc_data(col_data, cols + 2);
-    }
-
     void eee(
         Matrix<PairDouble> &payoff_matrix,
         std::vector<double> &row_strategy,
@@ -112,17 +52,17 @@ namespace LRSNash
 
         solve(&g, row_data, col_data);
 
-        double x{1 / mpz_get_d(row_data[0])};
+        double x{1 / *row_data[0]};
         for (int row_idx = 0; row_idx < rows; ++row_idx)
         {
-            row_strategy[row_idx] = mpz_get_d(row_data[row_idx + 1]) * x;
+            row_strategy[row_idx] = *row_data[row_idx + 1] * x;
         }
 
-        double y{1 / mpz_get_d(col_data[0])};
+        double y{1 / *col_data[0]};
 
         for (int col_idx = 0; col_idx < cols; ++col_idx)
         {
-            col_strategy[col_idx] = mpz_get_d(col_data[col_idx + 1]) * y;
+            col_strategy[col_idx] = *col_data[col_idx + 1] * y;
         }
 
         dealloc_data(row_data, rows + 2);
