22b1cfddef928a4df79edb43fab1ba64059f03e7
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-07-07 09:19:46 -0700

    working node.cc

--- tests/cart/node.cc
+++ tests/cart/node.cc
@@ -2,34 +2,43 @@
 
 #include <tuple>
 
-struct NodeWrapper {
-
-    template <typename BanditAlgorithm>
-    struct MNode : MatrixNode<BanditAlgorithm> {
-
-    };
-
-    template <typename BanditAlgorithm>
-    struct CNode : ChanceNode<BanditAlgorithm> {
-
-    };
-
-};
-
 template <template <typename> typename... Template>
 struct TemplatePack {};
 
 template <typename... Type>
 struct TypePack {};
 
-template <
-    template <typename, template <typename> typename, template <typename> typename> typename TreeBanditTemplate, 
-    typename NodeTypes, 
-    typename... BanditTypes>
-auto buzz (std::tuple<BanditTypes...> bandit_tuple) -> 
-        std::tuple<TreeBanditTemplate<BanditTypes, typename NodeTypes::template MNode, typename NodeTypes::template CNode>...>{
-    return {std::make_from_tuple<TreeBanditTemplate<BanditTypes, typename NodeTypes::template MNode, typename NodeTypes::template CNode>>(bandit_tuple)...};
+struct NodeWrapper {
+
+    template <typename BanditAlgorithm>
+    using MNode = MatrixNode<BanditAlgorithm>;
+
+    template <typename BanditAlgorithm>
+    using CNode = ChanceNode<BanditAlgorithm>;
 };
+
+struct NodeWrapper2 {
+
+    template <typename BanditAlgorithm>
+    using MNode = MatrixNodeL<BanditAlgorithm>;
+
+    template <typename BanditAlgorithm>
+    using CNode = ChanceNodeL<BanditAlgorithm>;
+};
+
+
+template <
+    template <typename, template <typename> typename, template <typename> typename> typename TreeBanditTemplate,
+    typename NodeTypes,
+    typename... BanditTypes>
+auto buzz(std::tuple<BanditTypes...> bandit_tuple) -> std::tuple<TreeBanditTemplate<BanditTypes, NodeTypes::template MNode, NodeTypes::template CNode>...>
+{
+    return std::apply([](auto... bandits) {
+        return std::tuple<TreeBanditTemplate<BanditTypes, NodeTypes::template MNode, NodeTypes::template CNode>...>(
+            TreeBanditTemplate<BanditTypes, NodeTypes::template MNode, NodeTypes::template CNode>(bandits)...
+        );
+    }, bandit_tuple);
+}
 
 template <
     template <typename, template <typename> typename, template <typename> typename> typename TreeBanditTemplate, 
@@ -58,10 +67,23 @@ int main () {
 
     Exp3<Model> bandit{.01};
 
-    std::tuple<Exp3<Model>> bandit_tuple{bandit};
-    TypePack<NodeWrapper> node_type_pack{};
+    std::tuple<Exp3<Model>, Exp3<Model>> bandit_tuple{bandit, bandit};
+    TypePack<NodeWrapper, NodeWrapper2> node_type_pack{};
 
     auto x = foo<TreeBandit>(bandit_tuple, node_type_pack);
+    auto count = std::tuple_size_v<decltype(x)>;
+    std::cout << count << std::endl;
+    
+    // auto y = std::get<0>(x);
+
+    // MoldState<9> state{10};
+    // Model model{0};
+    // MatrixNode<TreeBandit<Exp3<Model>, NodeWrapper::MNode, NodeWrapper::CNode>>  root{}; 
+
+    // prng device{0};
+
+    // y.run(1000, device, state, model, root);
+
 
     return 0;
 }
