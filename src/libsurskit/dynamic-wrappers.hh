35b7aa4cd4226ca6aaf8f283c3bc18d42d0ffc30
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-08-08 21:59:06 -0700

    more wip

--- src/libsurskit/dynamic-wrappers.hh
+++ src/libsurskit/dynamic-wrappers.hh
@@ -48,40 +48,54 @@ namespace W
             virtual bool is_terminal() const = 0;
             virtual Types::Value get_payoff() const = 0;
         };
-        template <IsPerfectInfoStateTypes T>
-        struct StateWrapper : State
+
+        // template <
+        //     IsTypeList TypeList,
+        //     template <typename...> typename StateTypes,
+        //     typename... Args>
+        // auto make_state(Args &&...args) -> StateT<TypeList, StateTypes>
+        // {
+        //     return StateT ()
+        // }re
+
+        template <
+            IsTypeList TypeList,
+            template <typename...> typename StateTypes>
+            requires IsPerfectInfoStateTypes<StateTypes<TypeList>>
+        struct StateT : State
         {
-            typename T::State state;
+            using T = StateTypes<TypeList>;
+            typename T::State data;
 
             template <typename... Args>
-            StateWrapper(Args... args) : state{args...} {}
+            StateT(Args... args) : data{args...} {}
 
             std::unique_ptr<Detail::State> clone() const
             {
-                return std::make_unique<StateWrapper>(*this);
+                return std::make_unique<StateT>(*this);
             }
 
             void get_actions(size_t &rows, size_t &cols)
             {
-                state.get_actions();
-                rows = state.row_actions.size();
-                cols = state.col_actions.size();
+                data.get_actions();
+                rows = data.row_actions.size();
+                cols = data.col_actions.size();
             };
             void apply_actions(typename Types::Action row_idx, typename Types::Action col_idx)
             {
-                state.apply_actions(
-                    state.row_actions[static_cast<int>(row_idx)],
-                    state.col_actions[static_cast<int>(col_idx)]);
+                data.apply_actions(
+                    data.row_actions[static_cast<int>(row_idx)],
+                    data.col_actions[static_cast<int>(col_idx)]);
             };
             bool is_terminal() const
             {
-                return state.is_terminal();
+                return data.is_terminal();
             };
             Types::Value get_payoff() const
             {
                 return Types::Value{
-                    static_cast<double>(state.payoff.get_row_value()),
-                    static_cast<double>(state.payoff.get_col_value())};
+                    static_cast<double>(data.payoff.get_row_value()),
+                    static_cast<double>(data.payoff.get_col_value())};
             }
         };
 
@@ -90,24 +104,29 @@ namespace W
             virtual std::unique_ptr<Detail::Model> clone() const = 0;
             virtual void get_inference(State *, Types::ModelOutput &) = 0;
         };
-        template <IsValueModelTypes T>
-        struct ModelWrapper : Model
+        template <
+            IsTypeList TypeList,
+            template <typename...> typename StateTypes,
+            template <typename...> typename ModelTypes>
+            requires(IsValueModelTypes<ModelTypes<StateTypes<TypeList>>>)
+        struct ModelT : Model
         {
+            using T = ModelTypes<StateTypes<TypeList>>;
             typename T::Model model;
 
             template <typename... Args>
-            ModelWrapper(Args... args) : model{args...} {}
+            ModelT(Args... args) : model{args...} {}
 
             std::unique_ptr<Detail::Model> clone() const
             {
-                return std::make_unique<ModelWrapper>(*this);
+                return std::make_unique<ModelT>(*this);
             }
 
             void get_inference(
                 State *state_ptr,
                 Types::ModelOutput &output)
             {
-                typename T::State &state = dynamic_cast<StateWrapper<T> *>(state_ptr)->state;
+                typename T::State &state = dynamic_cast<typename T::State *>(state_ptr)->data;
                 typename T::ModelOutput output_;
                 model.get_inference(state, output_);
                 output.value = Types::Value{output_.value.get_row_value(), output_.value.get_col_value()};
@@ -144,7 +163,7 @@ namespace W
         struct Search
         {
             virtual std::unique_ptr<Detail::Search> clone() const = 0;
-            virtual std::unique_ptr<Detail::MatrixNode> get_matrix_node () const = 0;
+            virtual std::unique_ptr<Detail::MatrixNode> get_matrix_node() const = 0;
             virtual size_t run(
                 size_t duration_ms,
                 Types::PRNG &device,
@@ -158,20 +177,28 @@ namespace W
                 Detail::Model *model_ptr,
                 Detail::MatrixNode *matrix_node_ptr) const = 0;
         };
-        template <IsSearchTypes T>
-        struct SearchWrapper : Search
+
+        template <
+            IsTypeList TypeList,
+            template <typename...> typename StateTypes,
+            template <typename...> typename ModelTypes,
+            template <typename...> typename SearchTypes>
+            requires(IsSearchTypes<SearchTypes<ModelTypes<StateTypes<TypeList>>>>)
+        struct SearchT : Search
         {
+            using T = SearchTypes<ModelTypes<StateTypes<TypeList>>>;
             typename T::Search session;
 
             template <typename... Args>
-            SearchWrapper(Args... args) : session{args...} {}
+            SearchT(Args... args) : session{args...} {}
 
             std::unique_ptr<Detail::Search> clone() const
             {
-                return std::make_unique<SearchWrapper<T>>(*this);
+                return std::make_unique<SearchT<T>>(*this);
             }
 
-            std::unique_ptr<Detail::MatrixNode> get_matrix_node () const {
+            std::unique_ptr<Detail::MatrixNode> get_matrix_node() const
+            {
                 return std::make_unique<MatrixNodeWrapper<T>>();
             }
 
@@ -183,8 +210,8 @@ namespace W
                 Detail::MatrixNode *matrix_node_ptr) const
             {
                 typename T::PRNG device_{device.uniform_64()};
-                typename T::State &state = dynamic_cast<StateWrapper<T> *>(state_ptr)->state;
-                typename T::Model &model = dynamic_cast<ModelWrapper<T> *>(model_ptr)->model;
+                typename T::State &state = dynamic_cast<StateT<T> *>(state_ptr)->state;
+                typename T::Model &model = dynamic_cast<ModelT<T> *>(model_ptr)->model;
                 typename T::MatrixNode &matrix_node = dynamic_cast<MatrixNodeWrapper<T> *>(matrix_node_ptr)->matrix_node;
                 size_t iterations = session.run(duration_ms, device_, state, model, matrix_node);
                 return iterations;
@@ -193,8 +220,8 @@ namespace W
                 size_t iterations, Types::PRNG &device, Detail::State *state_ptr, Detail::Model *model_ptr, Detail::MatrixNode *matrix_node_ptr) const
             {
                 typename T::PRNG device_{device.uniform_64()};
-                typename T::State &state = dynamic_cast<StateWrapper<T> *>(state_ptr)->state;
-                typename T::Model &model = dynamic_cast<ModelWrapper<T> *>(model_ptr)->model;
+                typename T::State &state = dynamic_cast<StateT<T> *>(state_ptr)->state;
+                typename T::Model &model = dynamic_cast<ModelT<T> *>(model_ptr)->model;
                 typename T::MatrixNode &matrix_node = dynamic_cast<MatrixNodeWrapper<T> *>(matrix_node_ptr)->matrix_node;
                 size_t ms = session.run_for_iterations(iterations, device_, state, model, matrix_node);
                 return ms;
@@ -228,12 +255,8 @@ namespace W
             return *this;
         }
 
-        template <IsStateTypes T, typename... Args>
-        State(T, const Args &...args) : ptr(std::make_unique<Detail::StateWrapper<T>>(args...))
-        {
-            typename T::State &sss = (dynamic_cast<Detail::StateWrapper<T> *>(&*ptr))->state;
-            std::cout << "constr: max depth: " << sss.max_depth << std::endl;
-        }
+        template <IsTypeList TypeList, template <typename...> typename StateTypes, typename... Args>
+        State(const Args &...args) : ptr(std::make_unique<Detail::StateT<TypeList, StateTypes>>(args...)) {}
 
         bool is_terminal() const
         {
@@ -297,7 +320,7 @@ namespace W
         std::unique_ptr<Detail::Model> ptr;
 
         template <IsValueModelTypes T, typename... Args>
-        Model(T, const Args &...args) : ptr(std::make_unique<Detail::ModelWrapper<T>>(args...)) {}
+        Model(T, const Args &...args) : ptr(std::make_unique<Detail::ModelT<T>>(args...)) {}
 
         Model(const Model &other)
         {
@@ -330,7 +353,7 @@ namespace W
     {
         std::unique_ptr<Detail::MatrixNode> ptr;
 
-        MatrixNode (std::unique_ptr<Detail::MatrixNode> ptr) : ptr{std::move(ptr)} {}
+        MatrixNode(std::unique_ptr<Detail::MatrixNode> ptr) : ptr{std::move(ptr)} {}
 
         template <IsNodeTypes T>
         MatrixNode(T) : ptr{std::make_unique<Detail::MatrixNodeWrapper<T>>()} {}
@@ -341,7 +364,7 @@ namespace W
         std::unique_ptr<Detail::Search> ptr;
 
         template <IsSearchTypes T, typename... Args>
-        Search(T, const Args &...args) : ptr(std::make_unique<Detail::SearchWrapper<T>>(args...)) {}
+        Search(T, const Args &...args) : ptr(std::make_unique<Detail::SearchT<T>>(args...)) {}
 
         Search(const Search &other)
         {
@@ -354,7 +377,8 @@ namespace W
             return *this;
         }
 
-        MatrixNode get_matrix_node () const {
+        MatrixNode get_matrix_node() const
+        {
             return MatrixNode{ptr->get_matrix_node()};
         }
 
