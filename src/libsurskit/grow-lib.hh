c572597aaa1743e8835997138bfc44d8b253662d
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-08-25 14:50:07 -0700

    add alternating actions to growlib make ENABLE_CONCEPTS a compile def remove OneOne test state

--- src/libsurskit/grow-lib.hh
+++ src/libsurskit/grow-lib.hh
@@ -12,4 +12,25 @@ More functions for creating random trees. TODO
 
 namespace GrowLib
 {
+
+    template <typename RandomTree>
+    int alternating_constant_actions (RandomTree *state, int actions) {
+        int actions_to_move = std::max(state->rows, state->cols);
+        if (actions == 1) {
+            return actions_to_move;
+        } else {
+            return 1;
+        }
+    }
+
+    template <typename RandomTree, int min, int max>
+    int alternating_uniform_random_actions (RandomTree *state, int actions) {
+        int actions_to_move = std::max(state->rows, state->cols);
+        if (actions == 1) {
+            return state->device.uniform_int(std::max(min, 2), max + 1);
+        } else {
+            return 1;
+        }
+    }
+
 };
--- src/types/concepts.hh
+++ src/types/concepts.hh
@@ -6,8 +6,6 @@ Include to disable concepts to fix horrendous error messages
 
 */
 
-#define ENABLE_CONCEPTS
-
 #ifdef ENABLE_CONCEPTS
 #define CONCEPT(Concept, Types) Concept Types
 #else
