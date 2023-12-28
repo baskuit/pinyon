da8310987bcb602ac4725451b1f2e4f0359c1018
Ryan Keathley <ryan@pkmn.ai>
2023-12-28 00:44:26 -0700

    fulltraversal multithread hack

--- src/algorithm/solver/full-traversal.hh
+++ src/algorithm/solver/full-traversal.hh
@@ -7,6 +7,7 @@
 #include <model/model.hh>
 #include <tree/tree.hh>
 #include <algorithm/algorithm.hh>
+#include <thread>
 
 /*
     This algorithm expands a node into a tree that is one-to-one with the abstract game tree
@@ -19,7 +20,7 @@
 template <
     CONCEPT(IsSingleModelTypes, Types),
     template <typename...> typename NodePair = DefaultNodes>
-requires IsChanceStateTypes<Types> 
+    requires IsChanceStateTypes<Types>
 struct FullTraversal : Types
 {
     struct MatrixStats
@@ -31,11 +32,15 @@ struct FullTraversal : Types
         size_t matrix_node_count = 1;
         unsigned int depth = 0;
         typename Types::Prob prob;
+
+        typename Types::Mutex mutex{};
+        bool is_expanded = false;
     };
     struct ChanceStats
     {
         std::vector<typename Types::Obs> chance_actions;
         std::vector<typename Types::Prob> chance_strategy;
+        typename Types::Mutex mutex{};
     };
     using MatrixNode = typename NodePair<Types, MatrixStats, ChanceStats>::MatrixNode;
     using ChanceNode = typename NodePair<Types, MatrixStats, ChanceStats>::ChanceNode;
@@ -43,7 +48,6 @@ struct FullTraversal : Types
     class Search
     {
     public:
-
         Search() {}
 
         std::pair<typename Types::Real, typename Types::Real>
@@ -52,20 +56,31 @@ struct FullTraversal : Types
             Types::PRNG &,
             const Types::State &state,
             Types::Model &model,
-            MatrixNode &matrix_node) const
+            MatrixNode &matrix_node,
+            const size_t threads = 1) const
         {
             auto state_ = state;
-            auto pair = run_(max_depth, state_, model, &matrix_node);
-            return pair;
+            std::thread thread_pool[threads];
+            for (int i = 0; i < threads; ++i)
+            {
+                thread_pool[i] = std::thread(
+                    &Search::run_, this, max_depth, &state_, &model, &matrix_node);
+            }
+            for (int i = 0; i < threads; ++i)
+            {
+                thread_pool[i].join();
+            }
+            return {matrix_node.stats.payoff.get_row_value(), matrix_node.stats.payoff.get_row_value()};
         }
 
-        std::pair<typename Types::Real, typename Types::Real>
-        run_(
+        void run_(
             const size_t max_depth,
-            Types::State &state,
-            Types::Model &model,
+            Types::State *state_ptr,
+            Types::Model *model_ptr,
             MatrixNode *matrix_node) const
         {
+            typename Types::State &state = *state_ptr;
+            typename Types::Model &model = *model_ptr;
             // expand node
             state.get_actions();
             const size_t rows = state.row_actions.size();
@@ -78,7 +93,8 @@ struct FullTraversal : Types
             {
                 stats.payoff = state.get_payoff();
                 matrix_node->set_terminal();
-                return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};
+                // return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};            *output_real = stats.payoff.get_row_value();
+                return;
             }
             if (stats.depth >= max_depth)
             {
@@ -86,25 +102,47 @@ struct FullTraversal : Types
                 model.inference(std::move(state), output);
                 stats.payoff = output.value;
                 matrix_node->set_terminal();
-                return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};
+                // return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};
+                return;
             }
-            
-            stats.nash_payoff_matrix.fill(rows, cols);
-            stats.row_solution.resize(rows);
-            stats.col_solution.resize(cols);
+
+            stats.mutex.lock();
+            if (!stats.is_expanded)
+            {
+                stats.nash_payoff_matrix.fill(rows, cols);
+                stats.row_solution.resize(rows);
+                stats.col_solution.resize(cols);
+                for (int row_idx = 0; row_idx < rows; ++row_idx)
+                {
+                    for (int col_idx = 0; col_idx < cols; ++col_idx)
+                    {
+                        const typename Types::Action &row_action{state.row_actions[row_idx]};
+                        const typename Types::Action &col_action{state.col_actions[col_idx]};
+
+                        ChanceNode *chance_node = matrix_node->access(row_idx, col_idx);
+                    }
+                }
+            }
+            stats.is_expanded = true;
+            stats.mutex.unlock();
 
             // recurse
             for (int row_idx = 0; row_idx < rows; ++row_idx)
             {
                 for (int col_idx = 0; col_idx < cols; ++col_idx)
                 {
+                    const typename Types::Action &row_action{state.row_actions[row_idx]};
+                    const typename Types::Action &col_action{state.col_actions[col_idx]};
+
+                    ChanceNode *chance_node = matrix_node->access(row_idx, col_idx);
+
+                    if (!chance_node->stats.mutex.try_lock())
+                    {
+                        continue;
+                    }
 
                     std::cout << "FULL TRAVERSAL: " << row_idx << ' ' << col_idx << std::endl;
 
-                    const typename Types::Action &row_action{state.row_actions[row_idx]};
-                    const typename Types::Action &col_action{state.col_actions[col_idx]};
-                    
-                    ChanceNode *chance_node = matrix_node->access(row_idx, col_idx);
                     auto &chance_actions = chance_node->stats.chance_actions;
                     state.get_chance_actions(row_action, col_action, chance_actions);
 
@@ -118,18 +156,21 @@ struct FullTraversal : Types
                         matrix_node_next->stats.prob = state_copy.prob;
                         chance_node->stats.chance_strategy.push_back(state_copy.prob);
 
-                        auto value_ = run_(max_depth, state_copy, model, matrix_node_next);
+                        run_(max_depth, &state_copy, model_ptr, matrix_node_next);
 
-                        stats.nash_payoff_matrix.get(row_idx, col_idx) += 
-                            matrix_node_next->stats.payoff * 
+                        stats.nash_payoff_matrix.get(row_idx, col_idx) +=
+                            matrix_node_next->stats.payoff *
                             typename Types::Real{matrix_node_next->stats.prob};
                         stats.matrix_node_count += matrix_node_next->stats.matrix_node_count;
                     }
+
+                    chance_node->stats.mutex.unlock();
                 }
             }
 
             stats.payoff = LRSNash::solve(stats.nash_payoff_matrix, stats.row_solution, stats.col_solution);
-            return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};
+            // return {stats.payoff.get_row_value(), stats.payoff.get_row_value()};
+            return;
         }
     };
 };
