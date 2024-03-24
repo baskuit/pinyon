23fe4a08a3f67fbb992feb1b89359601683e9ff8
Ryan Keathley <me@ryankeathley.com>
2024-03-24 11:13:36 -0700

    some work on ab-iter test

--- tests/solving/ab-iter.cc
+++ tests/solving/ab-iter.cc
@@ -2,6 +2,40 @@
 
 #include <chrono>
 
+/*
+Takes model types (requires the model be 'state-invariant')
+and asserts that iterative deepening produces the same alpha/beta
+when solving on the same state and same depth
+
+We would also like to assert that the search requires less nodes
+but that is simply not going to be the case all the time
+
+*/
+
+using SolvedTree = TraversedState<NullModel<RandomTree<RandomTreeRationalTypes>>>;
+
+
+// struct ABIterTestData {
+//     size_t time_us;
+//     size_t iter_time_us;
+//     size_t count;
+//     size_t iter_count;
+// };
+
+// template <IsValueModelTypes Types>
+// struct IterTest {
+
+//     using Flat = AlphaBetaForce<Types>;
+//     using Iter = AlpbaBetaIter<Types>
+
+//     ABIterTestData test (const Types::State &) const {
+//         size_t time_us, iter_time_us, count, count_iter;
+        
+//         return {time_us, iter_time_us, count, count_iter};
+//     }
+
+// };
+
 using Types = AlphaBetaForce<MonteCarloModel<RandomTree<RandomTreeRationalTypes>>>;
 using IterTypes = AlphaBetaIter<MonteCarloModel<RandomTree<RandomTreeRationalTypes>>>;
 using FullTypes = FullTraversal<MonteCarloModel<RandomTree<RandomTreeRationalTypes>>>;
@@ -68,8 +102,8 @@ void solve_check(
     double time_ratio = iter_us / (double) ab_us;
     double count_ratio = count_iter / (double) count;
 
-    assert(node.alpha == node.alpha);
-    assert(node.beta == node.beta);
+    assert(node.alpha == node_iter.alpha);
+    assert(node.beta == node_iter.beta);
 
     std::cout << "count ratio: " << count_ratio << std::endl;
     std::cout << "time ratio: " << time_ratio << std::endl;
@@ -89,8 +123,19 @@ void solve_check(
     std::cout << std::endl;
 }
 
+SolvedTree::State generate_solved_tree (
+    prng &device,
+    const size_t depth,
+    const size_t actions,
+    const size_t transitions) {
+    NullModel<RandomTree<RandomTreeRationalTypes>>::Model model{};
+    return {{device, depth, actions, actions, transitions}, model};
+}
+
 int main()
 {
+
+    
 
     Types::Q threshold{0};
     RandomTreeGenerator<RandomTreeRationalTypes> generator{
