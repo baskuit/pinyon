1b2e745b57790ed108052767c7a9b5b3718156ad
Ryan Keathley <me@ryankeathley.com>
2024-03-29 21:12:00 -0700

    random-tree-expl: almost surely fixed now, just reenable stuff

--- tests/solving/random-tree-expl.cc
+++ tests/solving/random-tree-expl.cc
@@ -6,7 +6,20 @@ Test that tree bandit algorithms are producing low exploitability when searching
 
 */
 
-const double expl_threshold = .60;
+// TODO seems to be working again
+// but matrix ucb refined strategies does not. Also enabling all the other shit is annoying.
+
+// std::cout << search << std::endl;
+// std::cout << "matrix:" << std::endl;
+// payoff_matrix.print();
+// std::cout << "strategies:" << std::endl;
+// math::print(row_strategy);
+// math::print(col_strategy);
+// std::cout << "visits:" << std::endl;
+// math::print(root.stats.row_visits);
+// math::print(root.stats.col_visits);
+
+const double expl_threshold = .3;
 
 template <typename Types, typename SolvedState>
 void test_st(const typename Types::State &state, const SolvedState &solved_state) {
@@ -25,20 +38,11 @@ void test_st(const typename Types::State &state, const SolvedState &solved_state
     search.get_refined_strategies(root.stats, row_strategy, col_strategy);
     const auto refined_expl = math::exploitability(payoff_matrix, row_strategy, col_strategy);
 
-    // std::cout << search << std::endl;
-    // std::cout << "matrix:" << std::endl;
-    // payoff_matrix.print();
     std::cout << "expl: " << expl << std::endl;
     std::cout << "refined expl: " << refined_expl << std::endl;
 
-    // std::cout << "strategies:" << std::endl;
-    // math::print(row_strategy);
-    // math::print(col_strategy);
-    // std::cout << "visits:" << std::endl;
-    // math::print(root.stats.row_visits);
-    // math::print(root.stats.col_visits);
-
-    assert(expl < typename Types::Real{expl_threshold});
+    assert(expl < expl_threshold);
+    // assert(refined_expl < expl_threshold);
 }
 
 template <typename Types, typename SolvedState>
@@ -120,41 +124,13 @@ int main() {
     auto mtp_search_type_tuple = search_type_generator<TreeBanditThreadPool>(bandit_type_pack, node_template_pack);
     auto op_search_type_tuple = search_type_generator<OffPolicy>(bandit_type_pack, node_template_pack);
 
-    prng device{0};
-    BaseTypes::Model model(device);
+    BaseTypes::Model model{0};
 
-    RandomTreeGenerator<> generator{prng{5809346740957}, {1, 2}, {2}, {1, 3}, {0}, std::vector<size_t>(100, 0)};
+    RandomTreeGenerator<> generator{prng{0}, {1, 2}, {2}, {1, 3}, {0}, std::vector<size_t>(100, 0)};
 
     for (const auto &wrapped_state : generator) {
         const BaseTypes::State state = (wrapped_state.unwrap<BaseTypes>());
-        // std::cout << "state seed: " << state.device.get_seed() << std::endl;
         auto solved_state = TraversedState<BaseTypes>::State{state, model};
-        std::cout << "state size: " << solved_state.node->stats.matrix_node_count << std::endl;
-
-        // std::cout << "chance strategies:" << std::endl;
-        // math::print(state.chance_strategies);
-
-        // std::cout << "apply chance actions demo:" << std::endl;
-        // for (int i{}; i < state.rows; ++i) {
-        //     for (int j{}; j < state.cols; ++j) {
-        //         for (int c{}; c < 3; ++c) {
-        //             std::cout << "-" << i << ' ' << j << ' ';
-        //             auto state_copy{state};
-        //             state_copy.apply_actions(i, j, c);
-        //             std::cout << state_copy.prob << ' ' << state_copy.get_payoff() << std::endl;
-        //         }
-        //     }
-        // }
-
-        // std::cout << "apply actions demo:" << std::endl;
-        // for (int i{}; i < state.rows; ++i) {
-        //     for (int j{}; j < state.cols; ++j) {
-        //         std::cout << "+" << i << ' ' << j << ' ';
-        //         auto state_copy{state};
-        //         state_copy.apply_actions(i, j);
-        //         std::cout << state_copy.get_payoff() << std::endl;
-        //     }
-        // }
 
         test_expl_st(st_search_type_tuple, state, solved_state);
         // test_expl_mt(mt_search_type_tuple, state, solved_state);
